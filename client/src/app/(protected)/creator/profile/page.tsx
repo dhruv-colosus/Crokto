65b6b2106d89c0df5d449efdb743199eb0f84801
Dhruv <dhruvdeora31@gmail.com>
2024-10-09 20:22:52 +0530

    added profile

--- client/src/app/(protected)/creator/profile/page.tsx
+++ client/src/app/(protected)/creator/profile/page.tsx
@@ -1,5 +1,212 @@
-function Profile() {
-  return <div>page</div>;
-}
+import * as React from "react";
 
-export default Profile;
+import { File, ListFilter } from "lucide-react";
+
+import { Badge } from "@/components/ui/badge";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Progress } from "@/components/ui/progress";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Link from "next/link";
+
+export const description =
+  "An orders dashboard with a sidebar navigation. The sidebar has icon navigation. The content area has a breadcrumb and search in the header. The main area has a list of recent orders with a filter and export button. The main area also has a detailed view of a single order with order details, shipping information, billing information, customer information, and payment information.";
+
+export default function Profile() {
+  return (
+    <div className="flex min-h-screen w-full flex-col bg-muted/40 p-6">
+      <h1 className="text-4xl font-extrabold mb-4">Your Stats</h1>
+      <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
+        <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
+          <Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0">
+            <CardHeader className="pb-3">
+              <CardTitle>Your Courses</CardTitle>
+              <CardDescription className="text-balance max-w-lg leading-relaxed">
+                You can see your uploaded courses and manage them directly from
+                this different tab here with rich features
+              </CardDescription>
+            </CardHeader>
+            <CardFooter>
+              <Link href="/creator/manage">
+                {" "}
+                <Button>View My Courses</Button>
+              </Link>
+            </CardFooter>
+          </Card>
+          <Card x-chunk="dashboard-05-chunk-1">
+            <CardHeader className="pb-2">
+              <CardDescription> Total Revenue</CardDescription>
+              <CardTitle className="text-4xl">$1342.34</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-xs text-muted-foreground">
+                Last transaction was 4$
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Progress value={30} aria-label="30% increase" />
+            </CardFooter>
+          </Card>
+          <Card x-chunk="dashboard-05-chunk-2">
+            <CardHeader className="pb-2">
+              <CardDescription>No of Courses</CardDescription>
+              <CardTitle className="text-4xl">13</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-xs text-muted-foreground">
+                2 Added Last Month
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Progress value={12} aria-label="12% increase" />
+            </CardFooter>
+          </Card>
+        </div>
+        <Tabs defaultValue="week">
+          <div className="flex items-center">
+            <TabsList>
+              <TabsTrigger value="week">Week</TabsTrigger>
+              <TabsTrigger value="month">Month</TabsTrigger>
+              <TabsTrigger value="year">Year</TabsTrigger>
+            </TabsList>
+            <div className="ml-auto flex items-center gap-2">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="outline"
+                    size="sm"
+                    className="h-7 gap-1 text-sm"
+                  >
+                    <ListFilter className="h-3.5 w-3.5" />
+                    <span className="sr-only sm:not-sr-only">Filter</span>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuLabel>Filter by</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuCheckboxItem checked>
+                    Fulfilled
+                  </DropdownMenuCheckboxItem>
+                  <DropdownMenuCheckboxItem>Declined</DropdownMenuCheckboxItem>
+                  <DropdownMenuCheckboxItem>Refunded</DropdownMenuCheckboxItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+              <Button size="sm" variant="outline" className="h-7 gap-1 text-sm">
+                <File className="h-3.5 w-3.5" />
+                <span className="sr-only sm:not-sr-only">Export</span>
+              </Button>
+            </div>
+          </div>
+          <TabsContent value="week">
+            <Card x-chunk="dashboard-05-chunk-3">
+              <CardHeader className="px-7">
+                <CardTitle>Your Sales</CardTitle>
+                <CardDescription>Recent Buys for your course</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead>Customer</TableHead>
+
+                      <TableHead className="hidden sm:table-cell">
+                        Completed Status
+                      </TableHead>
+                      <TableHead className="hidden md:table-cell">
+                        Date
+                      </TableHead>
+                      <TableHead className="text-right">Amount</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    <TableRow className="bg-accent">
+                      <TableCell>
+                        <div className="font-medium">Liam Johnson</div>
+                        <div className="hidden text-sm text-muted-foreground md:inline">
+                          liam@example.com
+                        </div>
+                      </TableCell>
+
+                      <TableCell className="hidden sm:table-cell">
+                        <Badge className="text-xs" variant="secondary">
+                          Fulfilled
+                        </Badge>
+                      </TableCell>
+                      <TableCell className="hidden md:table-cell">
+                        2023-06-23
+                      </TableCell>
+                      <TableCell className="text-right">$250.00</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <div className="font-medium">Olivia Smith</div>
+                        <div className="hidden text-sm text-muted-foreground md:inline">
+                          olivia@example.com
+                        </div>
+                      </TableCell>
+
+                      <TableCell className="hidden sm:table-cell">
+                        <Badge className="text-xs" variant="outline">
+                          Declined
+                        </Badge>
+                      </TableCell>
+                      <TableCell className="hidden md:table-cell">
+                        2023-06-24
+                      </TableCell>
+                      <TableCell className="text-right">$150.00</TableCell>
+                    </TableRow>
+
+                    <TableRow>
+                      <TableCell>
+                        <div className="font-medium">Noah Williams</div>
+                        <div className="hidden text-sm text-muted-foreground md:inline">
+                          noah@example.com
+                        </div>
+                      </TableCell>
+
+                      <TableCell className="hidden sm:table-cell">
+                        <Badge className="text-xs" variant="secondary">
+                          Fulfilled
+                        </Badge>
+                      </TableCell>
+                      <TableCell className="hidden md:table-cell">
+                        2023-06-25
+                      </TableCell>
+                      <TableCell className="text-right">$350.00</TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </CardContent>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  );
+}
